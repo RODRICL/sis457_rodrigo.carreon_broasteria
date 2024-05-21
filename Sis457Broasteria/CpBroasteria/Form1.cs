534194097482f37fc88033b4ddc7ab3adf7230a2
RODRICL <carreonlambertinrodrigo@gmail.com>
2024-05-21 02:59:04 -0400

    diseño del formulario de login

--- Sis457Broasteria/CpBroasteria/Form1.cs
+++ Sis457Broasteria/CpBroasteria/Form1.cs
@@ -16,5 +16,10 @@ namespace CpBroasteria
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
